2842b5409a3c5f01ca33200eb9ecc1111d332a4c
nasirmehmood825 <nasirmehmood825@gmail.com>
2023-08-06 07:18:37 -0400

    completed CI-build

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,8 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -91,6 +93,8 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -118,9 +122,8 @@ def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
-       account.delete()
+        account.delete()
     return "", status.HTTP_204_NO_CONTENT
-# ... place you code here to DELETE an account ...
 
 
 ######################################################################
